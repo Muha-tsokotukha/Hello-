6187ca014359da27fdc904b5bb3dcdbb2a47cbf2
Muha-tsokotukha <m_nasyrov@kbtu.kz>
2021-01-27 21:41:49 +0600

    loops

---
+++ for.py
@@ -0,0 +1,13 @@
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    i += 1
+    continue
+  if i == 4:
+      break
+  i += 1
+
+berries = ["melon", "blackberry", "blah"]
+for x in berries:
+  print(x)
